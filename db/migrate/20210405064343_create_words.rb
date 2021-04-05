28b51e45662e12605b0ef150acdff4b238c39660
yonblog <77332211+yonblog@users.noreply.github.com>
2021-04-05 15:48:32 +0900

    wordsテーブルの作成

--- db/migrate/20210405064343_create_words.rb
+++ db/migrate/20210405064343_create_words.rb
@@ -1,7 +1,9 @@
 class CreateWords < ActiveRecord::Migration[6.0]
   def change
     create_table :words do |t|
-
+      t.string :name,        null: false
+      t.string :description, null: false
+      t.references :user,    null: false, foreign_key: true
       t.timestamps
     end
   end
