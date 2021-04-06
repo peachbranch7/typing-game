7ea22c9e8beb2289aa5f518fb813ef1b80f5d2e3
peachbranch777 <peachbranch.0517@gmail.com>
2021-04-06 19:03:52 +0900

    count_downタイマーjs実装

---
+++ app/javascript/count_down.js
@@ -0,0 +1,49 @@
+function count() {
+  var time = 60;
+  var counter;
+  var min = document.getElementById("min");
+  var sec = document.getElementById("sec");
+  var start = document.getElementById("start");
+  var stop = document.getElementById("stop");
+  
+
+  start.onclick = function() {
+    toggle();
+    counter = setInterval( count, 1000 );
+  }
+
+  stop.onclick = function() {
+    toggle();
+    clearInterval( counter );
+  }
+
+  function toggle() {
+    if( start.disabled ) {
+     start.disabled = false;
+     stop.disabled = true;
+   } else {
+     start.disabled = true
+     stop.disabled = false;
+     }
+   }
+
+   function count() {
+    if( time === 0 ) {
+    sec.innerHTML = 0;
+    min.innerHTML = 0;
+    toggle();
+    alert("3分経過しました。");
+    clearInterval( counter );
+    } else {
+    time -= 1;
+    sec.innerHTML = time % 60;
+    min.innerHTML = Math.floor( time / 60 );
+    }
+    }
+}
+
+
+
+
+
+window.addEventListener("load", count);
