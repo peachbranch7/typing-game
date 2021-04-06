f8c8d91517be2f27b1b9961f76e92100a71b513c
peachbranch777 <peachbranch.0517@gmail.com>
2021-04-06 17:50:31 +0900

    入力された文字列とデータベースの文字列を比較

--- app/javascript/typing.js
+++ app/javascript/typing.js
@@ -1,15 +1,28 @@
 function questions(){
   const question = document.getElementById("typing-words");
-  question.innerHTML = gon.word[0];
-};
+  // question.innerHTML = gon.word[0];
 
-const checkText = []
-checkTexts = gon.word[0].split('');
+
+var checkTexts = [];
+checkTexts = gon.word[0].split('').map(function(value) {
+  var span = document.createElement('span');
+  span.textContent = value;
+  question.appendChild(span);
+  // console.log(span);
+  return span;
+});
+
 console.log(checkTexts);
+
 
 document.addEventListener('keydown', keydown);
 
 function keydown(e) {
-}
+  if (e.key === checkTexts[0].textContent) {
+    checkTexts[0].className = 'add-blue';
 
-addEventListener("load", questions);
+  }
+
+}}
+
+window.addEventListener("load", questions);
