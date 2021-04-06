3fcbce42a0eeec5b2d051840afa732e16f9c936f
peachbranch777 <peachbranch.0517@gmail.com>
2021-04-06 18:17:47 +0900

    関数createTextsを定義し、問題が次々に出力されるように定義

--- app/javascript/typing.js
+++ app/javascript/typing.js
@@ -1,28 +1,44 @@
 function questions(){
   const question = document.getElementById("typing-words");
-  // question.innerHTML = gon.word[0];
 
 
-var checkTexts = [];
-checkTexts = gon.word[0].split('').map(function(value) {
-  var span = document.createElement('span');
-  span.textContent = value;
-  question.appendChild(span);
-  // console.log(span);
-  return span;
-});
 
-console.log(checkTexts);
+  var checkTexts = [];
 
 
-document.addEventListener('keydown', keydown);
+  function createTexts() {
+    var rnd = Math.floor(Math.random() * gon.word.length);
+    question.textContent = '';
+    checkTexts = gon.word[rnd].split('').map(function(value) {
+      var span = document.createElement('span');
+      span.textContent = value;
+      question.appendChild(span);
+      // console.log(span);
+      return span;
+  })};
 
-function keydown(e) {
-  if (e.key === checkTexts[0].textContent) {
-    checkTexts[0].className = 'add-blue';
+
+  checkTexts = gon.word[0].split('').map(function(value) {
+    var span = document.createElement('span');
+    span.textContent = value;
+    question.appendChild(span);
+    return span;
+  });
+
+  console.log(checkTexts);
+
+
+  document.addEventListener('keydown', keydown);
+
+  function keydown(e) {
+    if (e.key === checkTexts[0].textContent) {
+      checkTexts[0].className = 'add-blue';
+      
+      checkTexts.shift();
+      if (!checkTexts.length) createTexts();
+    }
 
   }
-
-}}
+}
 
 window.addEventListener("load", questions);
