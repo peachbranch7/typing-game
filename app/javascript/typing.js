326d190a479a2193e7f2379bdda723b1303fe4e0
yonblog <77332211+yonblog@users.noreply.github.com>
2021-04-12 18:07:31 +0900

    問題カウント機能の実装とデータ送信の処理

--- app/javascript/typing.js
+++ app/javascript/typing.js
@@ -4,43 +4,68 @@ function questions(){
 
 
   var checkTexts = [];
+  var checkIds = [];
+  var count = 0;
 
 
   function createTexts() {
+    count += 1;
+    console.log(count);
+    const countNumber = document.getElementById("count-number");
+    countNumber.textContent = count
     var rnd = Math.floor(Math.random() * gon.word.length);
     question.textContent = '';
-    checkTexts = gon.word[rnd].split('').map(function(value) {
+
+    // ↓ここでIDを格納している
+    gon.ids = checkIds.push(gon.word[rnd].id);
+    console.log(checkIds);
+    
+    
+    
+    checkTexts = gon.word[rnd].name.split('').map(function(value) {
       var span = document.createElement('span');
       span.textContent = value;
       question.appendChild(span);
       // console.log(span);
       return span;
-  })};
-
-
-  checkTexts = gon.word[0].split('').map(function(value) {
-    var span = document.createElement('span');
-    span.textContent = value;
-    question.appendChild(span);
-    return span;
-  });
-
-  console.log(checkTexts);
-
-
-  document.addEventListener('keydown', keydown);
-
-  function keydown(e) {
-    if (e.key === checkTexts[0].textContent) {
-      checkTexts[0].className = 'add-blue';
-      
-      checkTexts.shift();
-      if (!checkTexts.length) createTexts();
+    })};
+    
+    
+    gon.ids = checkIds.push(gon.word[0].id);
+    checkTexts = gon.word[0].name.split('').map(function(value) {
+      var span = document.createElement('span');
+      span.textContent = value;
+      question.appendChild(span);
+      return span;
+    });
+    
+    
+    
+    document.addEventListener('keydown', keydown);
+    
+    function keydown(e) {
+      if (e.key === checkTexts[0].textContent) {
+        checkTexts[0].className = 'add-blue';
+        
+        checkTexts.shift();
+        if (!checkTexts.length) createTexts();
+      }
     }
-
-  }
-
-  
-}
-
-window.addEventListener("load", questions);
+    // let count = 0;
+    // count += 1
+    // console.log(count);
+    window.addEventListener("load", post);
+    function post() {
+      const submit = document.getElementById("end_btn")
+      end_btn.addEventListener("click", () => {
+      const form = document.getElementById("form")
+      const formData = new FormData(form)
+      const XHR = new XMLHttpRequest();
+      XHR.open("POST", "/scores", true);
+      XHR.responseType = "json";
+      XHR.send(formData);
+      })
+    };
+    
+  };
+  window.addEventListener("load", questions);
