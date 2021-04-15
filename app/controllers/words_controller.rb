659333af8ce10ed4a67c8d643da635685a0ceed2
yonblog <77332211+yonblog@users.noreply.github.com>
2021-04-15 23:01:33 +0900

    newアクションとcreateアクションの処理の記述と未ログインユーザーの処理

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,11 +1,28 @@
 class WordsController < ApplicationController
+  before_action :move_to_index, only: [:new, :create]
+
   def new
+    @word = Word.new
   end
 
   def create
+    @word = Word.new(word_params)
+    # binding.pry
+    if @word.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
   def word_params
-    params.require(:word).permit(:word, :description).merge(user_id: current_user.id)
+    params.require(:word).permit(:name, :description).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
